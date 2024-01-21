427d0d48e02fdbe003660e664b80b9abaa7bc570
Poornima5433 <128997033+Poornima5433@users.noreply.github.com>
2024-01-21 19:30:59 +0530

    Add files via upload
    21.1.2024_Qno1_Python: if else
    21.1.2021_Qno2_Python : Division

---
+++ pythoncoding1.py
@@ -0,0 +1,56 @@
+# Task
+
+# Given an integer, , perform the following conditional actions:
+
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2 to 5 , print Not Weird
+# If  is even and in the inclusive range of 6 to 20 , print Weird
+# If  is even and greater than 20, print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Constraints
+
+# Output Format
+
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# Weird
+# Explanation 0
+
+
+# n is odd and odd numbers are weird, so print Weird.
+
+# Sample Input 1
+
+# n=24
+# Sample Output 1
+
+# Not Weird
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n%2 == 0):
+        if (2<=n<=5) or (n>20):
+            print("Not Weird")
+        elif(6<=n<=20):
+            print("Weird")
+    else:
+        print("Weird")
